26f1dcbc56ad5921d9158173ba35b1151fbb2cd7
rahulkeezhath <rahulkeezhath@gmail.com>
2023-01-27 10:16:49 +0530

    Admin Dashboard Created

--- frontend/src/pages/admin/Dashboard/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import './Dashboard.css'
+import styled from 'styled-components'
+import DashboardContent from '../../../components/admin/DashboardContent/DashboardContent'
+import Sidebar from '../../../components/admin/Sidebar/Sidebar'
+
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
+  return(
+  <Div style={{backgroundColor:"#797979"}}>
+    <Sidebar/>
+    <DashboardContent/>
+  </Div>
   )
-}
+  }
+
+  const Div = styled.div`
+  position: relative;
+  `;
 
 export default Dashboard
